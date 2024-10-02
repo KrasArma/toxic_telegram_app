9b0fac08875b7b98112c3fccae1205724bad8282
KrasArma <dngshngmarker@gmail.com>
2024-10-02 10:55:03 +0300

    PeerChanel type add

--- gg.py
+++ gg.py
@@ -1,8 +1,7 @@
-
 import json
 import re
 from telethon import TelegramClient, events
-from telethon.errors import SessionPasswordNeededError
+from telethon.tl.types import PeerUser, PeerChat, PeerChannel
 from telethon.tl.functions.messages import SendReactionRequest
 from telethon.tl.types import ReactionEmoji
 
@@ -15,35 +14,46 @@ api_hash = credentials['api_hash']
 phone_number = credentials['phone_number']
 chat_ids = credentials['chat_ids']
 
-keywords = ['обед', 'курить', 'смочить', 'смокиш', 'парить']
 
-client = TelegramClient('user', api_id, api_hash)
+
+client = TelegramClient('new_user_session_bb', api_id, api_hash)
+
+
+keywords = ['обед', 'курить', 'смочить', 'смокиш', 'парить', "попарить", "перекур", "перекурить"]
 
 async def main():
     await client.start(phone_number)
-    
+
+    if not await client.is_user_authorized():
+
+        await client.sign_in(phone_number)
+        
 
     @client.on(events.NewMessage)
     async def handler(event):
+        peer_id = event.message.peer_id
 
-        if event.message.peer_id.user_id not in chat_ids and event.message.peer_id.channel_id not in chat_ids:
+        if isinstance(peer_id, PeerUser) and peer_id.user_id not in chat_ids:
+            return
+        if isinstance(peer_id, PeerChat) and peer_id.chat_id not in chat_ids:
+            return
+        if isinstance(peer_id, PeerChannel) and peer_id.channel_id not in chat_ids:
             return
 
         sender = await event.get_sender()
+
         if sender.id == (await client.get_me()).id:
             return
 
         message_text = event.message.message.lower()
 
-
         contains_keyword = any(re.search(rf'\b{keyword}\b', message_text, re.IGNORECASE) for keyword in keywords)
-
 
         if not contains_keyword:
             await client(SendReactionRequest(
                 peer=event.message.peer_id,
                 msg_id=event.message.id,
-                reaction=[ReactionEmoji(emoticon="👎")] 
+                reaction=[ReactionEmoji(emoticon="👎")]  
             ))
 
         if contains_keyword:
